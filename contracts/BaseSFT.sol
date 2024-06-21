0a0521d66fb4b7cd624fab22ae3678348a1e8139
taxio <taxio.2029@gmail.com>
2024-06-21 14:20:38 +0900

    feat: add BaseSFT

---
+++ contracts/BaseSFT.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+
+contract BaseSFT is ERC1155, Ownable, Pausable {
+    constructor(
+        address owner_,
+        string memory uri_
+    ) ERC1155(uri_) Ownable(owner_) {
+        _pause();
+    }
+}
