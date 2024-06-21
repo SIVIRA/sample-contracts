988b07e0e9971faacf75ddaa75e8dcd6c1aa56e9
taxio <taxio.2029@gmail.com>
2024-06-21 14:27:31 +0900

    feat: minter

--- contracts/BaseSFT.sol
+++ contracts/BaseSFT.sol
@@ -7,6 +7,22 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 
 contract BaseSFT is ERC1155, Ownable, Pausable {
+    error InvalidMinter(address minter);
+    error MinterAlreadyAdded(address minter);
+    error MintersFrozen();
+
+    event MinterAdded(address indexed minter);
+    event MinterRemoved(address indexed minter);
+
+    mapping(address minter => bool isMinter) internal _minters;
+    bool internal _isMintersFrozen;
+
+    modifier onlyMinter() {
+        require(_minters[_msgSender()], InvalidMinter(_msgSender()));
+
+        _;
+    }
+
     constructor(
         address owner_,
         string memory uri_
@@ -20,5 +36,40 @@ contract BaseSFT is ERC1155, Ownable, Pausable {
 
     function unpause() external onlyOwner {
         _unpause();
+    }
+
+    function addMinter(address minter_) external onlyOwner {
+        _requireMintersNotFrozen();
+
+        require(minter_ != address(0), InvalidMinter(address(0)));
+        require(!_minters[minter_], MinterAlreadyAdded(minter_));
+
+        _minters[minter_] = true;
+
+        emit MinterAdded(minter_);
+    }
+
+    function isMinter(address minter_) external view returns (bool) {
+        return _minters[minter_];
+    }
+
+    function removeMinter(address minter_) external onlyOwner {
+        _requireMintersNotFrozen();
+
+        require(_minters[minter_], InvalidMinter(minter_));
+
+        delete _minters[minter_];
+
+        emit MinterRemoved(minter_);
+    }
+
+    function freezeMinters() external onlyOwner {
+        _requireMintersNotFrozen();
+
+        _isMintersFrozen = true;
+    }
+
+    function _requireMintersNotFrozen() internal view {
+        require(!_isMintersFrozen, MintersFrozen());
     }
 }
