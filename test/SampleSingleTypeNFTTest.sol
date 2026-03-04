1a105f0c4a7bd280b9f62db9f31f86468342c466
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2026-03-04 21:58:39 +0900

    build: bump deps

--- test/SampleSingleTypeNFTTest.sol
+++ test/SampleSingleTypeNFTTest.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.33;
+pragma solidity 0.8.34;
 
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {IERC4906} from "@openzeppelin/contracts/interfaces/IERC4906.sol";
